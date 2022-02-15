ffd656c11e05c2f298ff77de8b035dc77c49e47b
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-02-15 01:03:26 -0500

    Access denied

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package org.ada.school.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,20 +8,38 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity( securedEnabled = true , jsr250Enabled = true , prePostEnabled = true )
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity( securedEnabled = true, jsr250Enabled = true, prePostEnabled = true )
+public class SecurityConfiguration
+        extends WebSecurityConfigurerAdapter
+{
+
+    JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfiguration( @Autowired JwtRequestFilter jwtRequestFilter )
+    {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Override
-    protected void configure (HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers( HttpMethod.GET, "/user" ).permitAll()
-                .antMatchers( HttpMethod.POST,"/user" ).permitAll()
-                .antMatchers(HttpMethod.POST, "/auth").permitAll()
-                .anyRequest().authenticated()
+    protected void configure( HttpSecurity http )
+            throws Exception
+    {
+        http.addFilterBefore( jwtRequestFilter,
+                        BasicAuthenticationFilter.class )
+                .cors()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers( HttpMethod.POST, "/auth" )
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS );
     }
 }
